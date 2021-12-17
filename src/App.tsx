f825950f0d338ee09db065a97e2c95c23d555745
EGOR KICHUK <kichukegor1997@gmail.com>
2021-12-17 21:36:06 +0300

    Redux-sage configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import {Main, Secondary, Error} from "./pages";
 import {theme} from "./styles/Theme.styled";
 import {GlobalStyles} from "./styles/Global.styled";
 
+import { rootSaga } from './store/rootSaga'
+import { sagaMiddleware } from './store/store'
+
 const App = () => {
+  sagaMiddleware.run(rootSaga)
 
   return (
     <ThemeProvider theme={theme}>
---
+++ src/store/rootSaga.ts
@@ -0,0 +1,5 @@
+import {call, fork } from 'redux-saga/effects'
+
+export function* rootSaga() {
+   /* yield call([fork(jobsListSaga), fork(jobDescriptionSaga)])*/
+}
