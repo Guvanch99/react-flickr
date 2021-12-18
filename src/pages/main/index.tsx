7db2d7954672bcd06c3869b3c6988f68cee418e7
Guvanch99 <awediyewguwanc@gmail.com>
2021-12-18 07:07:28 -0800

    Fetching data

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,15 @@
-import CardList from "../../components/card-list";
+import {useEffect} from "react";
 
-const Main = () => <CardList/>
+import CardList from "../../components/card-list";
+import {DB} from "../../core/axios";
+
+const Main = () => {
+  useEffect(() => {
+    DB('https://api.flickr.com/services/feeds/photos_public.gne?&format=json&nojsoncallback=true').then((data: any) => {
+      console.log(data.data)
+    })
+  }, [])
+  return <CardList/>
+}
 
 export default Main
